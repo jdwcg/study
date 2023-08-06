3ca9a080ce1b82055b35e5675efeb28aff2c18e8
jdwcg <bigcloudjo@gmail.com>
2023-08-06 20:55:20 +0900

    nextjs글생성

--- framework/nextprj001/app/page.js
+++ framework/nextprj001/app/page.js
@@ -5,6 +5,10 @@ export default function Home() {
     <>
       <h2>welcome</h2>
       Hello, web
+      <br />
+      <div className="foo">
+        <img src="sp_main.e43f76ed.png" alt="icons" />
+      </div>
     </>
   );
 }
